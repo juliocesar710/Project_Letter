01f99573e4f4147dce7be9e218f59652fc21340b
Júlio César <104685831+juliocesar710@users.noreply.github.com>
2025-04-13 12:22:01 -0300

    feat: add router to delete user (#15)

---
+++ Backend/src/repositories/userDeleteRepository.js
@@ -0,0 +1,25 @@
+import prisma from "../utils/prismaClient.js";
+
+export const deleteUserRepository = async (userId) => {
+  try {
+    
+    await prisma.genresFromUser.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
+    
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+
+   
+
+    return deletedUser;
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    throw new Error("Could not delete user");
+  }
+};
