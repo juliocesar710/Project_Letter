012544423760c489b5a339959160109a1707d7d5
julio candeia <juliocandeia594@gmail.com>
2025-04-22 12:57:45 -0300

    feat: create route to get a post of user

--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -4,13 +4,18 @@ import { genreTextGet } from "../controllers/GenresText/genreTextGetController.j
 import { getAllGenresText } from "../controllers/GenresText/genreTextGetAllController.js";
 import { createPost } from '../controllers/Posts/postMakeController.js';
 import { deletePost } from '../controllers/Posts/postDeleteController.js';
+import { getPostById } from '../controllers/Posts/postGetController.js';
+
+
 
 const router = express.Router();
 
 
 router.get("/genreText", authMiddleware, genreTextGet);
 router.get("/genreTextAll", getAllGenresText);
+
 router.post("/post", authMiddleware, createPost);
 router.delete("/postDelete/:postId", authMiddleware, deletePost);
+router.get("/postGet/:postId", authMiddleware, getPostById);
 
 export default router;
