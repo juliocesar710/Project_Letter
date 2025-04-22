0ae755c477fc8c01cce86170b1c278a24c475090
julio candeia <juliocandeia594@gmail.com>
2025-04-22 07:54:03 -0300

    feat: create route to user delete a post

--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -3,6 +3,7 @@ import authMiddleware from "../middlewares/auth.js";
 import { genreTextGet } from "../controllers/GenresText/genreTextGetController.js";
 import { getAllGenresText } from "../controllers/GenresText/genreTextGetAllController.js";
 import { createPost } from '../controllers/Posts/postMakeController.js';
+import { deletePost } from '../controllers/Posts/postDeleteController.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/genreText", authMiddleware, genreTextGet);
 router.get("/genreTextAll", getAllGenresText);
 router.post("/post", authMiddleware, createPost);
+router.delete("/postDelete/:postId", authMiddleware, deletePost);
 
 export default router;
