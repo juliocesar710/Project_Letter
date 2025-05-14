fbb02641e87c1788eb760e350464b2c3de109d51
julio candeia <juliocandeia594@gmail.com>
2025-05-14 16:53:02 -0300

    feat: created route to getUsers liked post

--- Backend/src/routes/like.js
+++ Backend/src/routes/like.js
@@ -3,11 +3,13 @@ import express from "express";
 import authMiddleware from "../middlewares/auth.js";
 import { likeController } from "../controllers/Like/likePostController.js";
 import { unlikeController } from "../controllers/Like/likeDeleteController.js";
+import { getUsersWhoLikedPostController } from "../controllers/Like/likeGetUsersController.js";
 
 const router = express.Router();
 
 router.post("/:postId", authMiddleware, likeController);
 router.delete("/:postId", authMiddleware, unlikeController);
+router.get("/:postId/users", authMiddleware, getUsersWhoLikedPostController);//futuramente deve-se pensar se vai ser ou nao publica esta rota
 
 
 
