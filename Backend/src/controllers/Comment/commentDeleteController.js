7a0061ca26e6f509c82f7a9585a3c99264bbf0ad
julio candeia <juliocandeia594@gmail.com>
2025-05-16 18:32:28 -0300

    feat: created route to view comments from post

--- Backend/src/controllers/Comment/commentDeleteController.js
+++ Backend/src/controllers/Comment/commentDeleteController.js
@@ -5,7 +5,7 @@ export const commentDeleteController = async (req, res) => {
     const userId = req.user.id;
     const { commentId } = req.params;
     await commentDeleteService(userId, Number(commentId));
-    res.status(204).send();
+    res.status(204).send().message("Comment deleted successfully");
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- Backend/src/routes/comment.js
+++ Backend/src/routes/comment.js
@@ -2,11 +2,15 @@ import express from "express";
 import authMiddleware from "../middlewares/auth.js";
 import { commentCreateController } from "../controllers/Comment/commentCreateController.js";
 import { commentDeleteController } from "../controllers/Comment/commentDeleteController.js";
+import { commentGetByPostController } from "../controllers/Comment/commentGetByPostController.js";
+
 
 const router = express.Router();
 
 router.post("/:postId", authMiddleware, commentCreateController);
 router.delete("/:commentId", authMiddleware, commentDeleteController);
+router.get("/post/:postId", commentGetByPostController);
+
 
 
 export default router;
